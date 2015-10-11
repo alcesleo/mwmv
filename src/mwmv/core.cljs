590cd03b4f6301f5ca028eb19212e30b15785aa8
Jimmy Börjesson <lagginglion@gmail.com>
2015-10-11 12:59:10 +0200

    Refactor core

--- src/mwmv/core.cljs
+++ src/mwmv/core.cljs
@@ -3,19 +3,13 @@
 
 (enable-console-print!)
 
-; ;; define your app data so that it doesn't get over-written on reload
-; (defonce app-state (atom {:text "Hello world!"}))
-
-; (defn on-js-reload []
-;   ;; optionally touch your app-state to force rerendering depending on your application
-;   (swap! app-state update-in [:__figwheel_counter] inc))
-
-(defn parse-string [s]    (subs s 1 (dec (count s))))
-(defn parse-date   [date] (js/Date. (str/join "-" (reverse (str/split date #"/")))))
-(defn parse-float  [f]    (.parseFloat js/window f))
-(defn round-cents  [f]    (/ (Math/round (* 100 f)) 100))
+(defn parse-string [s] (subs s 1 (dec (count s))))
+(defn parse-date   [d] (js/Date. (str/join "-" (reverse (str/split d #"/")))))
+(defn parse-float  [f] (.parseFloat js/window f))
+(defn round-cents  [f] (/ (Math/round (* 100 f)) 100))
 
 (defn parse-row
+  "Turns a row in the csv into a map"
   [row]
   (let [columns (str/split row #",") ; FIXME: Entries with commas in the description will fail
         [_ date category description _ _ _ _ _ _ _ amount _] columns]
@@ -25,44 +19,56 @@
      :amount      (parse-float amount)}))
 
 (defn parse-csv
-    [csv]
-    (->> csv
-         (str/split-lines)
-         (rest) ; Drop header row
-         (map parse-row)))
+  [csv]
+  (->> csv
+       (str/split-lines)
+       (rest) ; Drop header row
+       (map parse-row)))
 
 (defn yearmonth
-  "Returns a vector like [2015 2] from a date object in Feb 2015"
+  "Returns a vector like [2015 1] from a date object in Feb 2015"
   [date]
   [(.getFullYear date) (.getMonth date)])
 
-(def get-yearmonth "Returns the yearmonth of an entry" (comp yearmonth :date))
+(def get-yearmonth
+  "Returns the yearmonth of an entry"
+  (comp yearmonth :date))
 
 (defn yearmonth->str
   "A human readable yearmonth"
   [[year month]]
-  (let [months ["Jan" "Feb" "Mars" "Apr" "May" "Jun" "Jul" "Aug" "Sep" "Oct" "Nov" "Dec"]]
+  (let [months ["Jan" "Feb" "Mar" "Apr" "May" "Jun" "Jul" "Aug" "Sep" "Oct" "Nov" "Dec"]]
     (str (months month) " " year)))
 
 (defn get-timespan
   "Returns a timespan covering exactly the provided entries"
   [entries]
   (->> entries
-       (map (comp yearmonth :date))
+       (map get-yearmonth)
        (distinct)
        (sort)))
 
-(defn total-per-month
+(defn total-expense
+  "The total expense of entries (income is negative)"
+  [entries]
+  (->> entries
+       (map :amount)
+       (map -)
+       (reduce + 0)
+       (round-cents)))
+
+(defn total-expense-for-month
+  "The total expense of entries in yearmonth"
+  [entries yearmonth]
+  (->> entries
+       (filter #(= yearmonth (get-yearmonth %)))
+       (total-expense)))
+
+(defn total-expense-per-month
   "Total expenses for every month in timespan"
   [entries timespan]
   (map
-    (fn [yearmonth]
-      (->> entries
-        (filter #(= yearmonth (get-yearmonth %)))
-        (map :amount)
-        (map -) ; Invert to show expenses as positive
-        (reduce + 0)
-        (round-cents)))
+    (partial total-expense-for-month entries)
     timespan))
 
 (defn columns
@@ -71,50 +77,47 @@
   (->> entries
        (group-by :category)
        (map (fn [[category entries]]
-              (cons category (total-per-month entries timespan))))))
-
+              (cons category (total-expense-per-month entries timespan))))))
 
 
 ;;; ------------------- JavaScript stuff ---------------------
 
 
-
 (def chart-element-selector "#chart")
+(def file-input-element     (-> js/document (.getElementById "csv")))
+(def file-reader            (js/FileReader.))
 
 (defn generate-line-chart
-  [entries timespan]
-  (clj->js
+  "Create the JavaScript structure that c3 needs to render a line graph"
+  ([entries]
+   (generate-line-chart entries (get-timespan entries)))
+  ([entries timespan]
+   (clj->js
     {
      :bindto chart-element-selector
      :data   { :columns (columns entries timespan) }
      :axis   {
               :x {
                   :type "category"
-                  :categories (map yearmonth->str timespan)}}}))
+                  :categories (map yearmonth->str timespan)}}})))
 
 (defn render-chart
+  "Render the chart in the dom"
   [chart]
   (.generate js/c3 chart))
 
-
-(def file-input (-> js/document (.getElementById "csv")))
-(def reader     (js/FileReader.))
-
-; Load data into graph
+; Load data into graph when file has been read
 (.addEventListener
-  reader
+  file-reader
   "load"
-  #(render-chart
-     (let [entries (parse-csv (.-result reader))]
-      (generate-line-chart
-        entries
-        (get-timespan entries)))))
+  (fn []
+    (render-chart
+      (generate-line-chart (parse-csv (.-result file-reader))))))
 
-; Read file
+; Read file when chosen
 (.addEventListener
-  file-input
+  file-input-element
   "change"
   (fn [e]
-    (let [file (-> file-input (.-files) (aget 0))]
-      (println (.-name file))
-      (.readAsText reader file))))
+    (let [file (-> file-input-element (.-files) (aget 0))]
+      (.readAsText file-reader file))))
